81400cec5b36df97668ace0338a310694dd6dbd5
Gordon_Palagi <89727193+GordonPalagi@users.noreply.github.com>
2023-09-25 20:31:16 -0700

    added css and jsx

--- FrntEndMentor/src/App.jsx
+++ FrntEndMentor/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import png from './assets/image-qr-code.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='card'>
+      <img src={png} alt="improve your frontend skill by builing projects" />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h4>Improve your front-end skills by building projects</h4>
+        <p>Scan the Git code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
